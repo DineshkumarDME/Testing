be9d12afe71db5347c5389e6ee92413e9d2b2b27
K Dinesh Kumar <dinesh.kumar@i-exceed.com>
2025-05-28 15:21:49 +0530

    first commit

---
+++ src/main/java/com/jules/project/Jasper1Application.java
@@ -0,0 +1,13 @@
+package com.jules.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jasper1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jasper1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jules/project/Jasper1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jules.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jasper1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
